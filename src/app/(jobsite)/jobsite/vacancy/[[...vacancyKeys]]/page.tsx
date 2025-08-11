463555d4dbdd938b9cd5283986446555ae3fc69f
NeLeNik <lnek10.1990@gmail.com>
2025-08-11 16:49:31 +0300

    feat: added json-ld markup for google for jobs

--- src/app/(jobsite)/jobsite/vacancy/[[...vacancyKeys]]/page.tsx
+++ src/app/(jobsite)/jobsite/vacancy/[[...vacancyKeys]]/page.tsx
@@ -1,8 +1,40 @@
+import { vacancyEpmpoymentDict } from '@/entities/vacancy';
 import { PubVacDetails } from '@/pages-layer/pub-vac-details';
 import { getPubVacancy } from '@/shared/api/actions/public-vacancy';
+import { TPublicVacancy } from '@/shared/api/types';
 import { encodeSegment } from '@/shared/lib/encodeSegments';
 import { GoBackLink } from '@/shared/ui/GoBackLink';
+import { addMonths, format } from 'date-fns';
 import { redirect } from 'next/navigation';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const generateJsonLd = (vacancyData: TPublicVacancy) => {
+  const dateFromServer = new Date(vacancyData.publication_at || Date.now())
+  const dateValidThrough = addMonths(dateFromServer, 3)
+  const publicationAt = format(dateFromServer, 'MM/dd/yyyy')
+  const validThrough = format(dateValidThrough, 'MM/dd/yyyy')
+
+  const jsonLd = {
+    "@context": "http://schema.org",
+    "@type": "JobPosting",
+    "title": vacancyData.name,
+    "hiringOrganization": vacancyData.company,
+    "jobLocation": {
+      "@type": "Place",
+      "address": vacancyData.location
+    },
+    "datePosted": publicationAt,
+    "validThrough": validThrough,
+    "employmentType": vacancyData.employment ? vacancyEpmpoymentDict[vacancyData.employment] : '',
+    "baseSalary": {
+      "@type": "MonetaryAmount",
+      "currency": "₽",
+      "value": vacancyData.salary_from
+    },
+    "description": vacancyData.description || vacancyData.skills
+  }
+  return jsonLd
+}
 
 const JobsiteVacancyDetails = async ({
   params,
@@ -28,11 +60,20 @@ const JobsiteVacancyDetails = async ({
 
   return (
     <div>
+      {/* UNCOMMENT WHEN GET DATA FOR REAL VACANCY */}
+      {/* JSON-LD google for job markup */}
+      {/* <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify(generateJsonLd(vacancy)).replace(/</g, '\\u003c'),
+        }}
+      /> */}
+
       <GoBackLink
         text='К вакансиям'
       />
       <PubVacDetails vacancy={vacancy} />;
-    </div>
+    </div >
   )
 };
 
